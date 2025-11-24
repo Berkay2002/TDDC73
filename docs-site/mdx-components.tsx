211123f184dd02aefafffb764912ceff4aceacc2
Berkay Orhan <berkayorhan@hotmail.se>
2025-11-24 02:32:50 +0100

    any fix

--- docs-site/mdx-components.tsx
+++ docs-site/mdx-components.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
 import { CollapsibleCode } from './components/CollapsibleCode'
 
